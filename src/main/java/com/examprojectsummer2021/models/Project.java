42daa2869f91791d821117834172abab2dcdc017
Anton <anto8214@stud.kea.dk>
2021-05-07 13:25:52 +0200

    Added 'Project' and 'Task' classes

--- src/main/java/com/examprojectsummer2021/models/Project.java
+++ src/main/java/com/examprojectsummer2021/models/Project.java
@@ -1,9 +1,30 @@
 package com.examprojectsummer2021.models;
 
 /**
- * @author Carsten
- * Time: 12.37
- * Date: 06/05/2021
+ * @author Anton
  */
+
 public class Project {
+
+    private String title;
+    private String description;
+    private int projectID;
+
+    public Project(String title, String description, int projectID) {
+        this.title = title;
+        this.description = description;
+        this.projectID = projectID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getProjectID() {
+        return projectID;
+    }
 }
--- src/main/java/com/examprojectsummer2021/models/Task.java
+++ src/main/java/com/examprojectsummer2021/models/Task.java
@@ -1,9 +1,30 @@
 package com.examprojectsummer2021.models;
 
 /**
- * @author Carsten
- * Time: 12.37
- * Date: 06/05/2021
+ * @author Anton
  */
+
 public class Task {
+
+    private String title;
+    private String description;
+    private int taskID; // for SQL
+
+    public Task(String title, String description, int taskID) {
+        this.title = title;
+        this.description = description;
+        this.taskID = taskID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getTaskID() {
+        return taskID;
+    }
 }

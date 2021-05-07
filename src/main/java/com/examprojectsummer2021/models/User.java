cb598561b140ea1ab16dfab310416e743885ffed
JuliusPanduro <j.panduro.r@hotmail.com>
2021-05-07 09:40:36 +0200

    First push and Commit of the User class.

--- src/main/java/com/examprojectsummer2021/models/User.java
+++ src/main/java/com/examprojectsummer2021/models/User.java
@@ -1,9 +1,44 @@
 package com.examprojectsummer2021.models;
 
 /**
- * @author Carsten
- * Time: 12.37
- * Date: 06/05/2021
+ * @author Julius Panduro
+ * Time: 09.25
+ * Date: 07/05/2021
  */
+
 public class User {
+
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String role;
+
+    public User(String firstname, String lastname, String username, String role) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.username = username;
+        this.role = role;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    //Made to get the fully user information, to reduce the numbers of method calls.
+    public String getFullUserInfo() {
+        return firstname + lastname + username + role;
+    }
+
 }

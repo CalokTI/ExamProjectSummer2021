2ad3ecfb3cf9cbe2ec495f5979e05c88df2979ca
CalokTI <72436270+CalokTI@users.noreply.github.com>
2021-05-10 09:21:03 +0200

    Folders with goose

--- src/main/java/com/examprojectsummer2021/controllers/LoginController.java
+++ src/main/java/com/examprojectsummer2021/controllers/LoginController.java
@@ -7,10 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.Date;
 
 /**
  * @author Carsten

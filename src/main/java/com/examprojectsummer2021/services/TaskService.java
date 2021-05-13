37562b15208c5cd91e578152b48a0a02bbb9d130
Anton <anto8214@stud.kea.dk>
2021-05-13 12:38:01 +0200

    Moved method around

--- src/main/java/com/examprojectsummer2021/services/TaskService.java
+++ src/main/java/com/examprojectsummer2021/services/TaskService.java
@@ -15,28 +15,6 @@ public class TaskService {
         //return userList;
     // ------ GETTERS ------ //
 
-    public ArrayList<User> getUsersFromProject(int projectID) {
 
-        ResultSet resultSet = userRepository.getUsersFromProject(projectID);
-
-        ArrayList<User> userList = null;
-
-        try {
-            while (resultSet.next()){
-                String username = resultSet.getString(1);    // username
-                String firstname = resultSet.getString(2);   // first_name
-                String lastname = resultSet.getString(3);    // last_name
-                String role = resultSet.getString(5);        // role
-
-                User tmpUser = new User(username,firstname,lastname,role);
-                userList.add(tmpUser);
-            }
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
-        return null;
-    }
 
 }
